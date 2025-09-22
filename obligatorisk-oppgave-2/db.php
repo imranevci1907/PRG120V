682f351a6ea0d60414ac711f1d1cdef4104b5594
imranevci1907 <imranevci1907@gmail.com>
2025-09-22 13:40:43 +0200

    git

--- obligatorisk-oppgave-2/db.php
+++ obligatorisk-oppgave-2/db.php
@@ -3,12 +3,10 @@ $host = "localhost";      // server
 $user = "root";           // brukernavn til databasen
 $pass = "";               // passord
 $db   = "prg120v";        // databasenavn
-
 // Oppretter tilkobling
 $conn = new mysqli($host, $user, $pass, $db);
-
 // Sjekk om det gikk bra
 if ($conn->connect_error) {
-    die("Feil ved tilkobling: " . $conn->connect_error);
+die("Feil ved tilkobling: " . $conn->connect_error);
 }
 ?>
