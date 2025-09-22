3300cba6e3fe27a6e31564d4c231d60ff9c3abbf
imranevci1907 <imranevci1907@gmail.com>
2025-09-22 13:43:57 +0200

    git

--- obligatorisk-oppgave-2/registrer-klasser.php
+++ obligatorisk-oppgave-2/registrer-klasser.php
@@ -1,18 +1,18 @@
 <?php include("db.php"); ?>
-
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $kode = $_POST["klassekode"];
-    $navn = $_POST["klassenavn"];
-    $studium = $_POST["studiumkode"];
-
-    $sql = "INSERT INTO klasse (klassekode, klassenavn, studiumkode) 
-            VALUES ('$kode', '$navn', '$studium')";
-
-    if ($conn->query($sql)) {
-        echo "<p style='color:green;'>Klasse registrert!</p>";
-    } else {
-        echo "<p style='color:red;'>Feil: " . $conn->error . "</p>";
+ $kode = $_POST["klassekode"];
+ $navn = $_POST["klassenavn"];
+ $studium = $_POST["studiumkode"];
+ $sql = "INSERT INTO klasse (klassekode, klassenavn, studiumkode) 
+ VALUES ('$kode', '$navn', '$studium')";
+ if ($conn->query($sql))
+     {
+     echo "<p style='color:green;'>Klasse registrert!</p>";
+    } 
+    else
+     {
+    echo "<p style='color:red;'>Feil: " . $conn->error . "</p>";
     }
 }
 ?>
