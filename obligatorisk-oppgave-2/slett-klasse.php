0bd306d2f80209a47b02ffea34d4e78fbacf27d9
imranevci1907 <imranevci1907@gmail.com>
2025-09-24 15:23:53 +0200

    g

--- obligatorisk-oppgave-2/slett-klasse.php
+++ obligatorisk-oppgave-2/slett-klasse.php
@@ -1,39 +1,45 @@
 <?php
 /*
-   Programmet sletter en klasse (og alle tilhørende studenter) fra databasen
+   Programmet sletter en klasse fra databasen
 */
 include("db-tilkobling.php");  /* kobling til database */
-
 ?>
 
-<h3>Slett klasse</h3>
+<!DOCTYPE html>
+<html lang="no">
+<head>
+  <meta charset="UTF-8">
+  <title>Slett klasse</title>
+  <!-- Koble til JavaScript-fila -->
+  <script src="funksjoner.js"></script>
+</head>
+<body>
+  <h3>Slett klasse</h3>
 
-<form method="post" action="">
-  Klassekode: 
-  <input type="text" name="klassekode" required>
-<input type="submit" value="Slett klasse" name="slettKlasseKnapp" onclick="return bekreft();">
-</form>
+  <form method="post" action="">
+    Klassekode:
+    <input type="text" name="klassekode" required>
+    <!-- Koble knappen til bekreft()-funksjonen -->
+    <input type="submit" value="Slett klasse" name="slettKlasseKnapp" onclick="return bekreft();">
+  </form>
 
-<?php
-if (isset($_POST["slettKlasseKnapp"])) {
-    $klassekode = $_POST["klassekode"];
+  <?php
+  if (isset($_POST["slettKlasseKnapp"])) {
+      $klassekode = $_POST["klassekode"];
 
-    if (!$klassekode) {
-        print("Du må skrive inn en klassekode");
-    } else {
-        // Først slett studenter som hører til klassen
-        $sqlStudenter = "DELETE FROM student WHERE klassekode='$klassekode';";
-        mysqli_query($db, $sqlStudenter);
+      if (!$klassekode) {
+          print("Du må skrive inn en klassekode");
+      } else {
+          $sqlSetning = "DELETE FROM klasse WHERE klassekode='$klassekode';";
+          $sqlResultat = mysqli_query($db, $sqlSetning) or die("Ikke mulig å slette data i databasen");
 
-        // Deretter slett selve klassen
-        $sqlKlasse = "DELETE FROM klasse WHERE klassekode='$klassekode';";
-        mysqli_query($db, $sqlKlasse) or die("Ikke mulig å slette data i databasen");
-
-        if (mysqli_affected_rows($db) > 0) {
-            print("Klasse med kode <b>$klassekode</b> og alle tilhørende studenter er slettet.");
-        } else {
-            print("Fant ingen klasse med kode <b>$klassekode</b>.");
-        }
-    }
-}
-?>
+          if (mysqli_affected_rows($db) > 0) {
+              print("Klasse med kode <b>$klassekode</b> er slettet.");
+          } else {
+              print("Fant ingen klasse med kode <b>$klassekode</b>.");
+          }
+      }
+  }
+  ?>
+</body>
+</html>
