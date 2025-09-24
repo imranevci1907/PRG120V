d27d8f5ff4e775f5c046656691a2b2ce4402ca21
imranevci1907 <imranevci1907@gmail.com>
2025-09-24 14:36:28 +0200

    git

---
+++ obligatorisk-oppgave-2/vis-alle-klasser.php
@@ -0,0 +1,25 @@
+<?php  
+/*
+   Programmet skriver ut alle registrerte klasser
+*/
+include("db-tilkobling.php");  /* tilkobling til database-serveren utført og valg av database foretatt */
+
+$sqlSetning = "SELECT * FROM klasse;";  
+$sqlResultat = mysqli_query($db, $sqlSetning) or die ("Ikke mulig å hente data fra databasen");  
+
+$antallRader = mysqli_num_rows($sqlResultat);  
+
+print ("<h3>Registrerte klasser</h3>");
+print ("<table border='1'>");  
+print ("<tr><th>Klassekode</th><th>Klassenavn</th><th>Studiumkode</th></tr>"); 
+
+for ($r = 1; $r <= $antallRader; $r++) {
+    $rad = mysqli_fetch_array($sqlResultat);  
+    $klassekode = $rad["klassekode"];    
+    $klassenavn = $rad["klassenavn"];    
+    $studiumkode = $rad["studiumkode"];  
+
+    print ("<tr><td>$klassekode</td><td>$klassenavn</td><td>$studiumkode</td></tr>");
+}
+print ("</table>"); 
+?>
