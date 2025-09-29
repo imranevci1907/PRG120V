7e67dfc8b5e5efcc41e42e4d1c88ab6db26982cb
imranevci1907 <imranevci1907@gmail.com>
2025-09-29 12:28:36 +0200

    git

--- obligatorisk-oppgave-2/dynamiske-funksjoner.php
+++ obligatorisk-oppgave-2/dynamiske-funksjoner.php
@@ -1,14 +1,25 @@
 <?php
+function listeboksKlassekode() {
+    include("db-tilkobling.php");
+    $sql = "SELECT * FROM klasse ORDER BY klassekode;";
+    $resultat = mysqli_query($db,$sql) or die("Feil ved henting av klasser: " . mysqli_error($db));
+
+    while ($rad = mysqli_fetch_array($resultat)) {
+        $kode = $rad["klassekode"];
+        $navn = $rad["klassenavn"];
+        print("<option value='$kode'>$kode - $navn</option>");
+    }
+}
+
 function listeboksStudent() {
     include("db-tilkobling.php");
     $sql = "SELECT * FROM student ORDER BY brukernavn;";
-    $resultat = mysqli_query($db,$sql);
+    $resultat = mysqli_query($db,$sql) or die("Feil ved henting av studenter: " . mysqli_error($db));
 
     while ($rad = mysqli_fetch_array($resultat)) {
         $brukernavn = $rad["brukernavn"];
         $fornavn    = $rad["fornavn"];
         $etternavn  = $rad["etternavn"];
-
         print("<option value='$brukernavn'>$brukernavn - $fornavn $etternavn</option>");
     }
 }
